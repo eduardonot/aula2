3ba700d4f767b63df4d6eee63e564c62a3c112cf
eduardo <eduardo_not@hotmail.com>
2021-04-14 11:36:29 -0300

    exercicio iniciado

---
+++ index.js
@@ -0,0 +1,7 @@
+//Criar lista de produtos
+//Produtos devem conter: Nome, Preço e Quantidade em Estoque
+//Filtrar todos os produtos que ainda tenham estoque
+//Ordenar por preço crescente
+//Ordenar alfabeticamente
+//Filtrar produtos em estoque, ordenando por preço e exibir a soma de todos os produtos
+
